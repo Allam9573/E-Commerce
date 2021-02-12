f95095b0e9f901c7268ca30dc0f8c6fdc80331dd
Allam9573 <aallamdaniel@gmail.com>
2021-02-12 16:25:07 -0600

    prueba de cards

--- src/index.js
+++ src/index.js
@@ -5,19 +5,33 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
 import React, { Fragment } from "react";
 import { MDBBtn } from "mdbreact";
+import NavbarPage from './components/Navbar';
+import Productos from './components/Products';
+import EcommercePage from './components/Ecommerce';
 
 const AppRender = () => {
     return (
         <Fragment>
-        <MDBBtn color="primary">Primary</MDBBtn>
-        <MDBBtn>Default</MDBBtn>
-        <MDBBtn color="secondary">Secondary</MDBBtn>
-        <MDBBtn color="success">Success</MDBBtn>
-        <MDBBtn color="info">Info</MDBBtn>
-        <MDBBtn color="warning">Warning</MDBBtn>
-        <MDBBtn color="danger">Danger</MDBBtn>
-        <MDBBtn color="purple">Click</MDBBtn>
-      </Fragment>
+
+            <NavbarPage />
+            <div className="container">
+                <MDBBtn color="primary">Primary</MDBBtn>
+                <MDBBtn>Default</MDBBtn>
+                <MDBBtn color="secondary">Secondary</MDBBtn>
+                <MDBBtn color="success">Success</MDBBtn>
+                <MDBBtn color="info">Info</MDBBtn>
+                <MDBBtn color="warning">Warning</MDBBtn>
+                <MDBBtn color="danger">Danger</MDBBtn>
+                <MDBBtn color="purple">Click</MDBBtn>
+            </div>
+
+            <div className="container">
+                <Productos />
+            </div>
+            <div className="container">
+                <EcommercePage />
+            </div>
+        </Fragment>
     )
 }
 
